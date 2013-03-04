f1372102047d8bbcffa29ef0c9df4445f7e7bb5c
seyfer <seyferseed@mail.ru>
2013-03-04 16:41:23 +0800

    Add feauture to limit left and right pages links in template.

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This is pretty much hacked up to support Kohana 3.2 so there are a few things to
 - Request, Route and route parameters dependency injection added
 - Current Request is used by default instead of the initial one ($_GET was used directly in < 3.2)
 - URL::query() has been removed, Pagination::query() added instead (HMVC support)
+- Add feauture to limit links in rendered html template.
--- config/pagination.php
+++ config/pagination.php
@@ -1,15 +1,19 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
 
 return array(
-
-	// Application defaults
-	'default' => array(
-		'current_page'      => array('source' => 'query_string', 'key' => 'page'), // source: "query_string" or "route"
-		'total_items'       => 0,
-		'items_per_page'    => 10,
-		'view'              => 'pagination/basic',
-		'auto_hide'         => TRUE,
-		'first_page_in_url' => FALSE,
-	),
-
+    // Application defaults
+    'default' => array(
+        // source: "query_string" or "route"
+        'current_page'      => array('source' => 'query_string', 'key'    => 'page'),
+        'total_items'       => 0,
+        'items_per_page'    => 10,
+        'view'              => 'pagination/basic',
+        'auto_hide'         => TRUE,
+        'first_page_in_url' => FALSE,
+        //if use limited template
+        'max_left_pages'    => 10,
+        'max_right_pages'   => 10,
+    ),
 );
